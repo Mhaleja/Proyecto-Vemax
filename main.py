53303be4217a798fd6871223d5568ba0fb66bd2c
vanesa1116 <brivaboy@gmail.com>
2026-03-11 23:58:25 +0000

    actualizar html

--- main.py
+++ main.py
@@ -1,36 +1,33 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
 
 app = FastAPI()
 
 usuarios = []
 
+class RegistroModel(BaseModel):
+    correo: str
+    usuario: str
+    password: str
+    edad: int
+
+class LoginModel(BaseModel):
+    correo: str
+    password: str
+
 @app.get("/")
 def home():
-    return {"mensaje": "Mi API está funcionando"}
-
-@app.get("/eventos")
-def listar_eventos():
-    return {"eventos": ["CONIITI 2024", "Taller React", "Charla IA"]}
+    return FileResponse("index.html")
 
 @app.post("/registro")
-def registrar(correo: str, usuario: str, contraseña: str, edad: int):
-
-    nuevo_usuario = {
-        "correo": correo,
-        "usuario": usuario,
-        "contraseña": contraseña,
-        "edad": edad
-    }
-
-    usuarios.append(nuevo_usuario)
-
+def registrar(datos: RegistroModel):
+    usuarios.append(datos.dict())
     return {"mensaje": "Usuario registrado"}
 
 @app.post("/login")
-def login(correo: str, contraseña: str):
-
+def login(datos: LoginModel):
     for usuario in usuarios:
-        if usuario["correo"] == correo and usuario["contraseña"] == contraseña:
+        if usuario["correo"] == datos.correo and usuario["password"] == datos.password:
             return {"mensaje": f"Bienvenido {usuario['usuario']}"}
-
     return {"mensaje": "Datos incorrectos"}
